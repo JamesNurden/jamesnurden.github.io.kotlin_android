ddaf5bcbb4e5fb635a8505a676509b1820502846
James <jnurden@outlook.com>
2024-09-11 17:23:48 +0100

    Initial commit

---
+++ app.kt
@@ -0,0 +1,84 @@
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
+
+@Composable
+fun KotlinApp() {
+    val navController = rememberNavController()
+    val systemUiController = rememberSystemUiController()
+    val scaffoldState = rememberScaffoldState()
+
+    val isSystemDark = isSystemInDarkTheme()
+    val statusBarColor = if (isSystemDark){
+        MaterialTheme.colors.primaryVariant
+    }else {
+        Color.Transparent
+    }
+    SideEffect {
+        systemUiController.setStatusBarColor(statusBarColor, darkIcons = !isSystemDark)
+    }
+
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = movieDestinations.find {
+        backStackEntry?.destination?.route == it.route ||
+                backStackEntry?.destination?.route == it.routeWithArgs
+    }?: Home
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            KotlinAppBar(
+                canNavigateBack = navController.previousBackStackEntry != null,
+                currentScreen = currentScreen
+            ) {
+                navController.navigateUp()
+            }
+        }
+    ) {innerPaddings ->
+        NavHost(
+            navController = navController,
+            modifier = Modifier.padding(innerPaddings),
+            startDestination = Home.routeWithArgs
+        ){
+            composable(Home.routeWithArgs){
+                val homeViewModel: HomeViewModel = koinViewModel()
+                HomeScreen(
+                    uiState = homeViewModel.uiState,
+                    loadNext = {
+                                     homeViewModel.loadNext(forceReload = it)
+                    },
+                    navigateToDetail = {
+                        navController.navigate(
+                            "${Detail.route}/${it.id}"
+                        )
+                    }
+                )
+            }
+
+            composable(Detail.routeWithArgs, arguments = Detail.arguments){
+                val appId = it.arguments?.getInt("appId") ?: 0
+                val detailViewModel: DetailViewModel = koinViewModel(
+                    parameters = { parametersOf(appId) }
+                )
+
+                DetailScreen(uiState = detailViewModel.uiState)
+            }
+        }
+
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.getByName<Jar>("jar"))
+}
